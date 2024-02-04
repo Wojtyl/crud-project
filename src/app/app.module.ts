74de47b8c77a6edd984395714a79c13895151f63
Bartosz Wojtal <bartek.wojtal17@gmail.com>
2024-02-04 08:08:26 +0100

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { UserCreatePageComponent } from './components/user-create-page/user-create-page.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UserCreatePageComponent,
+    UserListComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule,
